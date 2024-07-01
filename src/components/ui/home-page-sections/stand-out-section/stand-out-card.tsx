249c3993b7315b7ebfdc0a62a708d32993f871d8
dev-rathore <rathoredvd3001@gmail.com>
2024-07-01 10:40:47 +0530

    stand out section

---
+++ src/components/ui/home-page-sections/stand-out-section/stand-out-card.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { motion } from "framer-motion";
+
+interface StandOutCardProps {
+  imageUrl: string;
+  description: string;
+  title: string;
+  initialX: [number, number];
+  order?: [string, string];
+}
+
+const StandOutCard: React.FC<StandOutCardProps> = ({
+  imageUrl,
+  initialX,
+  description,
+  order = ['', ''],
+  title,
+}) => {
+  return (
+    <div className="flex flex-col md:flex-row px-2 md:px-0 gap-12 md:gap-20 items-center justify-center">
+      <motion.div
+        style={{
+          background: `url(${imageUrl})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+        initial={{ opacity: 0, x: initialX[0] }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{
+          duration: 0.5,
+          ease: "easeOut",
+        }}
+        className={`${order[1]} md:${order[0]} w-full md:min-w-96 min-h-96 bg-primary shadow-secondary-2xl bg-no-repeat bg-center`}
+      />
+      <motion.div
+        className={`${order[0]} md:${order[1]} w-full md:w-1/2 flex flex-col gap-4`}
+        initial={{ opacity: 0, x: initialX[1] }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{
+          duration: 0.5,
+          ease: "easeOut",
+        }}
+      >
+        <h3 className="text-3xl font-semibold">{title}</h3>
+        <p className="text-lg">{description}</p>
+      </motion.div>
+    </div>
+  );
+};
+
+export default StandOutCard;
---
+++ src/components/ui/home-page-sections/stand-out-section/stand-out.tsx
@@ -0,0 +1,32 @@
+import StandOutCard from "./stand-out-card";
+
+const StandOutSection = () => {
+  return (
+    <section className="min-h-screen flex flex-col gap-16 md:gap-36 overflow-hidden">
+      <h1 className="text-5xl md:text-6xl text-center font-semibold drop-shadow-primary">Why choose us?</h1>
+      <div className="flex flex-col gap-12 md:gap-36">
+        <StandOutCard
+          initialX={[-100, 100]}
+          imageUrl="/photography-illustration.jpeg"
+          title="Expert Instructors & Flexible Learning"
+          description="Learn from experienced professionals who are leaders in their fields, with the flexibility to access courses anytime, anywhere, and at your own pace."
+        />
+        <StandOutCard
+          initialX={[100, -100]}
+          imageUrl="/chef-illustration.jpeg"
+          title="Comprehensive Resources & Community Support"
+          description="Gain access to a wealth of resources, including video tutorials, downloadable materials, and interactive assignments, while benefiting from a supportive community that encourages peer-to-peer learning and collaboration."
+          order={['order-2', 'order-1']}
+        />
+        <StandOutCard
+          initialX={[-100, 100]}
+          imageUrl="/team-collaboration.jpeg"
+          title="Affordable Pricing"
+          description="Enjoy premium content and features at competitive prices, with various subscription options to fit your budget."
+        />
+      </div>
+    </section>
+  );
+};
+
+export default StandOutSection;
